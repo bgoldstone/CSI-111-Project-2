50c18186df39214f9b0328b96924dcabee40d180
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-08 15:28:04 -0400

    deleted field class

--- src/Field.java
+++ src/Field.java
@@ -1,5 +1,56 @@
-public class Field {
-    public Field() {
+import java.util.Scanner;
+import java.io.*;
 
+public class Field {
+    private File fileName;
+    private Scanner fileReader;
+    private int lifespan;
+    private Mound[][] field;
+    private int[] firstMushroom = new int[2];
+
+    public Field(Scanner scan) {
+        System.out.println("What is the file name?");
+        fileName = new File(scan.nextLine());
+        while (!fileName.exists()) {
+            System.out.println("Invalid file name! \nWhat is the correct file name?");
+            fileName = new File(scan.nextLine());
+        }
+        try {
+            fileReader = new Scanner(fileName, "UTF-8");
+            boolean firstFieldLine = true;
+            int rowNumber = 0;
+            String line;
+            while (fileReader.hasNextLine()) {
+                line = fileReader.nextLine();
+                if (line.contains("Lifespan")) {
+                    lifespan = Integer.parseInt(line.substring(line.indexOf("=") + 1));
+                }
+                if (line.contains("First Mushroom")) {
+                    String[] tmp = line.split("=")[1].split(",");
+                    firstMushroom[0] = Integer.parseInt(tmp[0]);
+                    firstMushroom[1] = Integer.parseInt(tmp[1]);
+                } else {
+                    String[] row = line.split(",");
+                    int columnNumber = 0;
+                    if (firstFieldLine) {
+                        field = new Mound[row.length][row.length];
+                        firstFieldLine = false;
+                    }
+                    for (String item : row) {
+                        field[rowNumber][columnNumber] = new Mound(Integer.parseInt(item));
+                        columnNumber++;
+                    }
+                    rowNumber++;
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+    public void simulate(){
+        while(true){
+
+        }
     }
 }
--- src/Mound.java
+++ src/Mound.java
@@ -8,12 +8,13 @@ public class Mound {
     public Mound(int amountOfNutrients) {
         this.amountOfNutrients = amountOfNutrients;
         mound = new LinkedList<Mushroom>();
+        numberOfSpores = 0;
     }
 
     /**
      * Adds one day to the life of a Mushroom
      */
-    public void addOneDay(int lifespan, String[] fields) {
+    public void addOneDay(int lifespan, Mound[][] fields) {
         for (Mushroom mushroom : mound) {
             mushroom.setNumberOfDays(mushroom.getNumberOfDays() + 1);
             if (mushroom.getNumberOfDays() == lifespan) {
@@ -23,13 +24,33 @@ public class Mound {
         }
     }
 
-    public void addSpores(String[] fields) {
+    public boolean addSpores(Mound[][] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields[i].length; j++) {
+                try {
+                    if (fields[i][j].amountOfNutrients > 0 && fields[i][j].numberOfSpores > 0) {
+                        fields[i - 1][j].numberOfSpores += 1;
+                        fields[i + 1][j].numberOfSpores += 1;
+                        fields[i][j - 1].numberOfSpores += 1;
+                        fields[i][j + 1].numberOfSpores += 1;
+                        growMushrooms(fields);
+                        return true;
+                    }
+                } catch (IndexOutOfBoundsException e) {
 
-
+                }
+            }
+        }
+        return false;
     }
 
-    public void growMushrooms(String[] fields) {
-        if (amountOfNutrients < 0) return;
+    public void growMushrooms(Mound[][] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; j < fields[i].length; j++) {
+                if(fields[i][j].numberOfSpores > 0){
+                    mound.add(new Mushroom(0));
+                }
+            }
+        }
     }
-
 }
