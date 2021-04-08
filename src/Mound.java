fe892ce7d737fc88f8140c1bca27fb05ee6347c3
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-08 15:56:48 -0400

    Changed Mound.java

--- src/Mound.java
+++ src/Mound.java
@@ -7,50 +7,95 @@ public class Mound {
 
     public Mound(int amountOfNutrients) {
         this.amountOfNutrients = amountOfNutrients;
-        mound = new LinkedList<Mushroom>();
-        numberOfSpores = 0;
+        mound = new LinkedList<>();
+        this.numberOfSpores = 0;
     }
 
     /**
      * Adds one day to the life of a Mushroom
      */
     public void addOneDay(int lifespan, Mound[][] fields) {
-        for (Mushroom mushroom : mound) {
-            mushroom.setNumberOfDays(mushroom.getNumberOfDays() + 1);
-            if (mushroom.getNumberOfDays() == lifespan) {
-                mound.remove(mushroom);
-                addSpores(fields);
-            }
-        }
-    }
-
-    public boolean addSpores(Mound[][] fields) {
         for (int i = 0; i < fields.length; i++) {
             for (int j = 0; j < fields[i].length; j++) {
-                try {
-                    if (fields[i][j].amountOfNutrients > 0 && fields[i][j].numberOfSpores > 0) {
-                        fields[i - 1][j].numberOfSpores += 1;
-                        fields[i + 1][j].numberOfSpores += 1;
-                        fields[i][j - 1].numberOfSpores += 1;
-                        fields[i][j + 1].numberOfSpores += 1;
-                        growMushrooms(fields);
-                        return true;
+                for (Mushroom mushroom : fields[i][j].getMound()) {
+                    mushroom.setNumberOfDays(mushroom.getNumberOfDays() + 1);
+                    if (mushroom.getNumberOfDays() == lifespan) {
+                        addSpores(mushroom, lifespan, fields, new int[]{i, j});
+                        fields[i][j].getMound().remove(mushroom);
                     }
-                } catch (IndexOutOfBoundsException e) {
-
                 }
             }
         }
-        return false;
     }
+
+    public void addSpores(Mushroom mushroom, int lifespan, Mound[][] fields, int[] position) {
+        try {
+            fields[position[0] - 1][position[1]].setNumberOfSpores(fields[position[0] - 1][position[1]].getNumberOfSpores() + 1);
+            fields[position[0] + 1][position[1]].setNumberOfSpores(fields[position[0] + 1][position[1]].getNumberOfSpores() + 1);
+            fields[position[0]][position[1] - 1].setNumberOfSpores(fields[position[0]][position[1] - 1].getNumberOfSpores() + 1);
+            fields[position[0]][position[1] + 1].setNumberOfSpores(fields[position[0]][position[1] + 1].getNumberOfSpores() + 1);
+        } catch (IndexOutOfBoundsException ignored) {
+        }
+
+        growMushrooms(fields);
+    }
+
+    public int getAmountOfNutrients() {
+        return amountOfNutrients;
+    }
+
+    public void setAmountOfNutrients(int amountOfNutrients) {
+        this.amountOfNutrients = amountOfNutrients;
+    }
+
+    public int getNumberOfSpores() {
+        return numberOfSpores;
+    }
+
+    public void setNumberOfSpores(int numberOfSpores) {
+        this.numberOfSpores = numberOfSpores;
+    }
+
+    public LinkedList<Mushroom> getMound() {
+        return mound;
+    }
+
+    public void setMound(LinkedList<Mushroom> mound) {
+        this.mound = mound;
+    }
+
 
     public void growMushrooms(Mound[][] fields) {
-        for (int i = 0; i < fields.length; i++) {
-            for (int j = 0; j < fields[i].length; j++) {
-                if(fields[i][j].numberOfSpores > 0){
-                    mound.add(new Mushroom(0));
+        for (Mound[] row : fields) {
+            for (Mound mushroom : row) {
+                if (mushroom.getNumberOfSpores() > 0) {
+                    getMound().add(new Mushroom(0));
                 }
             }
         }
+    }
+
+    public int getAmountOfNutrients() {
+        return amountOfNutrients;
+    }
+
+    public void setAmountOfNutrients(int amountOfNutrients) {
+        this.amountOfNutrients = amountOfNutrients;
+    }
+
+    public int getNumberOfSpores() {
+        return numberOfSpores;
+    }
+
+    public void setNumberOfSpores(int numberOfSpores) {
+        this.numberOfSpores = numberOfSpores;
+    }
+
+    public LinkedList<Mushroom> getMound() {
+        return mound;
+    }
+
+    public void setMound(LinkedList<Mushroom> mound) {
+        this.mound = mound;
     }
 }
