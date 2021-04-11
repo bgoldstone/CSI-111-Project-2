9906c4408cee4f923a2e8da326eb11f83cfe4fdc
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-10 20:42:04 -0400

    Moved back to PrintWriter

--- src/Field.java
+++ src/Field.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -13,9 +11,9 @@ public class Field {
     private int numberOfDays = 0;
     private int maxNumInDay = 0;
     private int maxDay = 0;
-    private PrintWriter pw;
+    private BufferedWriter bw;
 
-    public Field() {
+    public Field() throws IOException {
         //Prompts user for file name
         StringBuilder sb = new StringBuilder();
         System.out.println("What is the file name? ");
@@ -27,48 +25,42 @@ public class Field {
             fileName = new File(scan.nextLine());
             sb.append("What is the file name? ").append(fileName.getName()).append("\n");
         }
-        try {
-            pw = new PrintWriter(fileName.getName().substring(0, fileName.getName().indexOf(".")) + "_out.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.out);
-        }
-        pw.println(sb);
-        try {
-            //Reads in file
-            Scanner fileReader = new Scanner(fileName);
-            boolean firstFieldLine = true;
-            int rowNumber = 0;
-            String line;
-            //If file has more lines, keep reading
-            while (fileReader.hasNextLine()) {
-                line = fileReader.nextLine();
-                if (line.contains("Lifespan")) {
-                    lifespan = Integer.parseInt(line.substring(line.indexOf("=") + 1));
-                } else if (line.contains("First Mushroom")) {
-                    String[] tmp = line.split("=")[1].split(",");
-                    position[0] = Integer.parseInt(tmp[0]);
-                    position[1] = Integer.parseInt(tmp[1]);
-                } else {
-                    String[] row = line.split(",");
-                    int columnNumber = 0;
-                    if (firstFieldLine) {
-                        field = new Mound[row.length][row.length];
-                        firstFieldLine = false;
-                    }
-                    for (String item : row) {
-                        field[rowNumber][columnNumber] = new Mound(Integer.parseInt(item));
-                        columnNumber++;
-                    }
-                    rowNumber++;
+        bw = new BufferedWriter(new FileWriter(fileName.getName().substring(0, fileName.getName().indexOf(".")) + "_out.txt"));
+
+        bw.write(sb.toString());
+
+        //Reads in file
+        Scanner fileReader = new Scanner(fileName);
+        boolean firstFieldLine = true;
+        int rowNumber = 0;
+        String line;
+        //If file has more lines, keep reading
+        while (fileReader.hasNextLine()) {
+            line = fileReader.nextLine();
+            if (line.contains("Lifespan")) {
+                lifespan = Integer.parseInt(line.substring(line.indexOf("=") + 1));
+            } else if (line.contains("First Mushroom")) {
+                String[] tmp = line.split("=")[1].split(",");
+                position[0] = Integer.parseInt(tmp[0]);
+                position[1] = Integer.parseInt(tmp[1]);
+            } else {
+                String[] row = line.split(",");
+                int columnNumber = 0;
+                if (firstFieldLine) {
+                    field = new Mound[row.length][row.length];
+                    firstFieldLine = false;
                 }
+                for (String item : row) {
+                    field[rowNumber][columnNumber] = new Mound(Integer.parseInt(item));
+                    columnNumber++;
+                }
+                rowNumber++;
             }
-            fileReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.out);
         }
+        fileReader.close();
     }
 
-    public void simulate() {
+    public void simulate() throws IOException {
         boolean keepGoing = true;
         //Puts first mushroom on field
         field[position[0]][position[1]].growMushrooms(true);
@@ -106,7 +98,6 @@ public class Field {
             numberOfDays++;
         }
         summarize();
-        pw.close();
     }
 
     /**
@@ -129,13 +120,13 @@ public class Field {
      * Gives Maximum number of Mushrooms in the simulation and the day it happened on
      * plus the nutrient levels at the end of the simulation
      */
-    public void summarize() {
-        pw.print(String.format("The maximum number of mushrooms on a single day was %d on day %d ", maxNumInDay, numberOfDays));
-        pw.print("\nThe nutrients still remaining in the field looks like this:");
+    public void summarize() throws IOException {
+        bw.write(String.format("The maximum number of mushrooms on a single day was %d on day %d ", maxNumInDay, numberOfDays));
+        bw.write("\nThe nutrients still remaining in the field looks like this:");
         for (Mound[] row : field) {
-            pw.print("\n|");
+            bw.write("\n|");
             for (Mound mound : row) {
-                pw.print(String.format("%d|", mound.getAmountOfNutrients()));
+                bw.write(String.format("%d|", mound.getAmountOfNutrients()));
             }
         }
     }
@@ -143,14 +134,17 @@ public class Field {
     /**
      * Gives # of Mushrooms in each mound per day
      */
-    public void fieldState() {
-        pw.print("\nDay:" + numberOfDays);
+    public void fieldState() throws IOException{
+        bw.write("\nDay:" + numberOfDays);
         for (Mound[] row : field) {
-            pw.print("|");
+            bw.write("|");
             for (Mound mound : row) {
-                pw.print("M" + mound.getMound().size() + "|");
+                bw.write("M" + mound.getMound().size() + "|");
             }
-            pw.print("\n");
+            bw.write("\n");
         }
+    }
+    public void closeWriter() throws IOException{
+        bw.close();
     }
 }
--- src/MushroomFarmer.java
+++ src/MushroomFarmer.java
@@ -1,12 +1,15 @@
+import java.io.IOException;
+
 /**
  * Simulates farming of mushrooms
  */
 public class MushroomFarmer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Field field = new Field();
         //Simulates Farming
         field.simulate();
         //Summarizes Farming Results
         field.summarize();
+        field.closeWriter();
     }
 }
