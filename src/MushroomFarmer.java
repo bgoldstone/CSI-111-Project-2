c4998a21c57e3fac912d5155a3eb5eee1ebceb80
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-10 20:19:05 -0400

    Tried replacing with BufferedWriter

--- src/MushroomFarmer.java
+++ src/MushroomFarmer.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
+
 /**
  * Simulates farming of mushrooms
  */
 public class MushroomFarmer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Field field = new Field();
         //Simulates Farming
         field.simulate();
